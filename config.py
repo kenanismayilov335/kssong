2be9ffac17c44625f0eb4a942914e53ca29a6644
kenanismayilov335 <74872919+kenanismayilov335@users.noreply.github.com>
2021-10-08 22:35:03 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,4 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-CASH_API_KEY = None # Get one from https://www.alphavantage.co/support/#api-key
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
